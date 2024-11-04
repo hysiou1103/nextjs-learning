a622c31756ad67027b22d57f3adcd45e1775c7c9
molly.lin <hysiou1103@gmail.com>
2024-11-04 22:37:07 +0800

    Tutorial8-巢狀動態路由

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,11 @@
+export default function ReviewDetail({
+  params,
+}: {
+  params: { reviewId: string; productId: string };
+}) {
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}
+    </h1>
+  );
+}
