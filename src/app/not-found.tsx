c10155ad385714fe25ce91682ac249d24315285b
molly.lin <hysiou1103@gmail.com>
2024-11-04 23:25:37 +0800

    Tutorial10-Not Found Page 1. 可以客製化 not-found page，檔案名稱須照文件要求 2. 可以在程式碼中呼叫 notFound() ，控制於特定情境下轉到 not-found page 3. 當路由有自己的客製化 not-found page 時，會轉到自己的 not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Page not found</h2>
+      <p>cound not find requested result</p>
+    </div>
+  );
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  // reviewId 大於 1000 即導 not found page，注意這裡不用 return
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
