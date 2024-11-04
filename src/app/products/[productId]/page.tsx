9835a2dd1a7de2f897cef5ade87c5ac3c6e61c2a
molly.lin <hysiou1103@gmail.com>
2024-11-04 22:05:38 +0800

    Tutorial7-動態路由

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,13 @@
+export default function ProductDetail({
+  params,
+}: {
+  params: {
+    productId: string;
+  };
+}) {
+  return (
+    <>
+      <h1>Details about product {params.productId}</h1>
+    </>
+  );
+}
