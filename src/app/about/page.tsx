4935ad75e0d1993fd4b7c105bbbe1bbfc3b0d090
molly.lin <hysiou1103@gmail.com>
2024-11-11 22:45:52 +0800

    Revert "Tutorial17-Title Metadata (static & dynamic)"
    This reverts commit 42c5c5012e2c7aec579c14035ec5309593bfa361.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,3 @@
-// static metadata，會取代 root layout 的 metadata.title
-export const metadata = {
-  title: "About",
-};
-
 export default function About() {
   return <h1>about Page</h1>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,31 +1,10 @@
-import { Metadata } from "next";
-
-type Props = {
+export default function ProductDetail({
+  params,
+}: {
   params: {
     productId: string;
   };
-};
-
-// dynamic metadata，也可以是 async function
-// export const generateMetadata = ({ params }: Props): Metadata => {
-//   return {
-//     title: `Product ${params.productId}`,
-//   };
-// };
-
-// demo async generateMetadata
-export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
-  const title = await new Promise(resolve => {
-    setTimeout(() => {
-      resolve(`Iphone ${params.productId}`);
-    }, 100);
-  });
-  return {
-    title: `Product ${title}`,
-  };
-};
-
-export default function ProductDetail({ params }: Props) {
+}) {
   return (
     <>
       <h1>Details about product {params.productId}</h1>
