9ce2c2189968ab0fafce31cba10a5fe855fec9e9
molly.lin <hysiou1103@gmail.com>
2024-11-11 22:47:54 +0800

    Tutorial17-Routing Metadata (static & dynamic)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+// static metadata，會取代 root layout 的 metadata.title
+export const metadata = {
+  title: "About",
+};
+
 export default function About() {
   return <h1>about Page</h1>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,31 @@
-export default function ProductDetail({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+
+// dynamic metadata，也可以是 async function
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`,
+//   };
+// };
+
+// demo async generateMetadata
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetail({ params }: Props) {
   return (
     <>
       <h1>Details about product {params.productId}</h1>
