56e2925fd2d61b2806b6915094040330d5bcd854
molly.lin <hysiou1103@gmail.com>
2024-11-25 21:49:45 +0800

    Tutorial25-Recovering from Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,12 @@
 "use client";
 // 必須為 clinet component，可以接收 error 物件，
 // 這裡的 error.message 為我們在 [reviewId]/page.tsx 中的 throw new Error
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div>{error.message}</div>;
+// reset function 是去 rerender error content，若成功，error content 將被 page.tsx 的 content 替代
+// 若要使用 error function 則 page.tsx 也須轉為 client component
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
+  return (
+    <div>
+      {error.message} <button onClick={reset}>Try again</button>
+    </div>
+  );
 }
